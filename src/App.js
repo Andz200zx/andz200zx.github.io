9d45b0cc84a068bf0d2ac5a816710ad2b4bb59cb
Andrew Thompson <andz200zx@gmail.com>
2019-10-28 12:58:30 +0000

    test

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ class App extends React.Component {
         <header className="App-header">
           <p>page under construction</p>
           <p>made with 🖤 by andrew thompson</p>
+
+          <p>test</p>
         </header>
       </div>
     );
