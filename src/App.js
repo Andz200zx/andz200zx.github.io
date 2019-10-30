00c1f5637cd1eeabd276dae6cae317c277c968f2
Andrew Thompson <andz200zx@gmail.com>
2019-10-30 18:51:42 +0000

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import * as React from "react";
 import "./App.css";
 
+import Coffee from "./coffee";
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <p>page under construction</p>
-          <p>made with 🖤 by andrew thompson</p>
+          {/* <p>made with 🖤 by andrew thompson</p> */}
+
+          <Coffee />
         </header>
       </div>
     );
